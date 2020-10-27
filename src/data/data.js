5981856df53819d5af6aa81baf784f4f819deb47
EAhri <ecadenhead@ufl.edu>
2020-10-27 18:37:07 -0400

    Adding Starting src

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+import drills from './drills.json'
+
+const getData = () => {
+    let all_drills = [];
+    let id = 1;
+        drills.forEach(element => {
+            element["id"] = id
+            all_drills.push(element)
+            id++
+        });
+    return all_drills;
+};
+
+export { getData }
